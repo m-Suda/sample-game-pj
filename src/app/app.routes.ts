64622841cf23cc912f6dbd7edc9f2223be6081c0
masaki suda <masaki_suda@catalyst-system.jp>
2024-07-16 23:49:38 +0900

    探索ページを追加

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ExploreComponent } from './pages/explore/explore.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'explore',
+        component: ExploreComponent
+    }
+];
